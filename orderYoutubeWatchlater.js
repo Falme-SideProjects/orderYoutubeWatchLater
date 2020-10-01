080c7f3893279ac6bf9d825407bc3574dca88e30
Falme Streamless <Falme@users.noreply.github.com>
2020-10-01 17:48:13 -0300

    updated some tags, not working properly

--- orderYoutubeWatchlater.js
+++ orderYoutubeWatchlater.js
@@ -1,15 +1,15 @@
 //Limit of loop : How many Videos are in Watch later
-var totalVideos= document.getElementsByClassName("timestamp").length;
-var actual =0; //Number of actual index loop
+var totalVideos= document.getElementsByClassName("ytd-thumbnail-overlay-time-status-renderer").length;
+var actual =1; //Number of actual index loop
 var timeslooped = 0; //How many times have found the video in loop
 
 //Array to save duration of all videos
 var arrayDurations = new Array();
 
 //set values to array
-for(var a=0; a<totalVideos; a++){
+for(var a=1; a<totalVideos; a+=2){
 	//Set as duration of each video
-	arrayDurations[a] = parseInt((document.querySelectorAll('.timestamp')[a].children[0].innerHTML).replace(/\:/g, ''));
+	arrayDurations[a] = parseInt((document.querySelectorAll('.ytd-thumbnail-overlay-time-status-renderer')[a].innerHTML).replace(/\:/g, ''));
 }
 
 //sort in descending order
@@ -23,7 +23,7 @@ function startSearch(){
 	while(actual < totalVideos && !found){
 		console.log(actual);
 		found = reorder(actual);
-		actual++;
+		actual+=2;
 	}
 
 	if(actual < totalVideos)
@@ -49,15 +49,15 @@ function startSearch(){
 function reorder(a){
 
 	//Set SAVO to compare atual video time to array
-	var savo = parseInt((document.querySelectorAll('.timestamp')[actual].children[0].innerHTML).replace(/\:/g, ''));
+	var savo = parseInt((document.querySelectorAll('.ytd-thumbnail-overlay-time-status-renderer')[a].innerHTML).replace(/\:/g, ''));
 
 	//If matches
 	if(savo == arrayDurations[0]){
 		timeslooped++; //to do not begin all again
 
 		//get elements to move up
-		document.getElementsByClassName("pl-video-edit-more")[a].click();
-		document.getElementsByClassName("pl-video-edit-move-top")[a].click();
+		document.querySelectorAll("#contents button.style-scope.yt-icon-button")[Math.floor(a/2)].click();
+		document.querySelectorAll("ytd-menu-service-item-renderer")[3].click();
 		arrayDurations.shift(); //remove video time
 		//actual=timeslooped; //reset actual loop index
 		actual=0; //reset actual loop index
